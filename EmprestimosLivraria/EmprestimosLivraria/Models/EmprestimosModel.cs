4d27efe9f6ea060a183a3acd0b31db9339d6f462
Allison de Paulo Sousa Avila <allissonsousa7@outlook.com>
2024-08-22 22:54:16 -0300

    Novo projeto

---
+++ EmprestimosLivraria/EmprestimosLivraria/Models/EmprestimosModel.cs
@@ -0,0 +1,10 @@
+﻿namespace EmprestimosLivraria.Models
+{
+    public class EmprestimosModel
+    {
+        public int Id { get; set; }
+        public string Recbedor { get; set; }
+        public string LivroEmprestado { get; set; }
+        public DateTime DataEmprestimo { get; set; } = DateTime.Now;
+    }
+}
